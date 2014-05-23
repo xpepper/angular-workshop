b1d391c7313f8d44a3bd619d6fccc9a6b945533d
Gabriele Mittica <mittica.gabriele@gmail.com>
2014-05-23 14:49:40 +0200

    karma and readme

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -5,16 +5,10 @@
 describe('controllers', function(){
   beforeEach(module('myApp.controllers'));
 
-
   it('should ....', inject(function($controller) {
     //spec body
-    var myCtrl1 = $controller('MyCtrl1', { $scope: {} });
-    expect(myCtrl1).toBeDefined();
+    var testCtrl = $controller('TestCtrl1', { $scope: {} });
+    expect(testCtrl).toBeDefined();
   }));
-
-  it('should ....', inject(function($controller) {
-    //spec body
-    var myCtrl2 = $controller('MyCtrl2', { $scope: {} });
-    expect(myCtrl2).toBeDefined();
-  }));
+  
 });
