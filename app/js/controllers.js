86c81849a2434b65fff417816ddbbc7d8623346b
Gabriele Mittica <mittica.gabriele@gmail.com>
2014-05-23 13:20:01 +0200

    test page

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,6 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
-
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+  .controller('TestCtrl1', ['$scope', function($scope) {
 
   }]);
