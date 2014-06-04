5486f2e138c892f2fbc2df69eb276262c0536b13
vagrant <vagrant@angular.local>
2014-06-04 13:59:29 +0000

    using ngSanitize

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
   'ngRoute',
+  'ngSanitize',
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
