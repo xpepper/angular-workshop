f29b59edf6a0e648223334e474877b73448c980d
Pietro Di Bello <pietro.dibello@xpeppers.com>
2014-06-05 14:33:54 +0200

    Wiring to the real news service

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,43 +5,48 @@
 angular.module('myApp.controllers', [])
   .controller('HomeCtrl1', ['$scope', '$http', function($scope, $http) {
 
-	  $scope.results = [];
-	  $scope.newsOrder = "title";
+    $scope.results = [];
+    $scope.newsOrder = "title";
 
-	  $scope.erase = function() {
-		  $scope.results = [];
-	  };
+    $scope.erase = function() {
+      $scope.results = [];
+    };
 
-	  $scope.search = function(searchKey) {
+    $scope.search = function(searchKey) {
       $http.get("http://rest.test.corley.it/search/" + searchKey)
         .success(function(data, status, headers, configurations) {
           $scope.results = data;
         });
-	  };
+    };
 
   }])
-  .controller('LoginCtrl', ['$scope', '$location', 'User', function($scope, $location, User){
+  .controller('LoginCtrl', ['$scope', '$location', '$http', 'User', function($scope, $location, $http, User){
 
-	  $scope.login = function(credentials) {
-		  console.log("logging...");
-		  console.log(User.isConnected());
-		  User.setCredentials(credentials);
-		  console.log(User.isConnected());
-		  $location.path("/publish");
-		  //do something
-	  };
+    $scope.login = function(credentials) {
+      $http.post("http://rest.test.corley.it/login", credentials)
+        .error(function() { $scope.loginError = "invalid credentials" })
+        .success(function() {
+          User.setCredentials(credentials);
+          $location.path("/publish");
+        });
+    };
 
   }])
-  .controller('PublishCtrl', ['$scope', '$location', 'User', function($scope, $location, User){
+  .controller('PublishCtrl', ['$scope', '$location', '$http', 'User', function($scope, $location, $http, User){
 
-	  console.log(User.isConnected());
-	  if(!User.isConnected()) {
-		  $location.path("/login");
-	  }
+    console.log(User.isConnected());
+    if (!User.isConnected()) {
+      $location.path("/login");
+    }
 
-	  $scope.publish = function(news) {
-		  console.log(news);
-	  }
+    $scope.publish = function(news) {
+      console.log(news);
+      $http.post("http://rest.test.corley.it/news", news)
+        .success(function(){
+
+        });
+
+    }
 
   }])
   ;
--- app/js/services.js
+++ app/js/services.js
@@ -7,26 +7,31 @@
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
   value('version', 'step2')
-  .factory('User', [function() {
-	 
+  .factory('User', ['$http', function($http) {
+
 	  var user = {
 			  "username": "",
 			  "password": ""
 	  };
-	  
-	  user.setCredentials = function(username, password) {
-		user.username = username;
-		user.password = password;
+
+	  user.setCredentials = function(credentials) {
+		    user.username = credentials.username;
+		    user.password = credentials.password;
+        var token = CryptoJS.enc.Base64.stringify(
+          CryptoJS.enc.Utf8.parse(user.username + ":" + user.password)
+        );
+        $http.defaults.headers.common.Authorization = "Basic " + token;
+        console.log("Set " + token);
 	  };
-	  
+
 	  user.getUsername = function() {
 		  return user.username;
 	  };
-	  
+
 	  user.getPassword = function() {
 		  return user.password;
 	  };
-	  
+
 	  user.isConnected = function() {
 		  if(user.username) {
 			  return true;
@@ -35,7 +40,7 @@ angular.module('myApp.services', []).
 			  return false;
 		  }
 	  }
-	  
+
 	  return user;
-	  
+
   }]);
